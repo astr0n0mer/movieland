723348a3d33e0e8e172e23b0c9dddddbfb8b9ee0
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-05-12 18:09:47 +0530

    Create movieland v1

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import SearchIcon from "./assets/search.svg";
+import "./App.css";
+
+import MovieCard from "./components/MovieCard";
+
+const API_URL = `http://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_API_KEY
+}`;
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    setMovies(() => searchMovies("one piece"));
+  }, []);
+
+  async function searchMovies(title) {
+    const movieData = await fetch(`${API_URL}&s=${title}`)
+      .then((response) => response.json())
+      .then((data) => data.Search);
+
+    setMovies(movieData);
+  }
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <form
+        className="search"
+        onSubmit={(e) => {
+          e.preventDefault();
+          searchMovies(e.target.elements.searchTerm.value);
+        }}
+      >
+        <input type="text" name="searchTerm" placeholder="Search for movies" />
+        <button type="submit">
+          <img src={SearchIcon} alt="Search" />
+        </button>
+      </form>
+
+      <div className="container">
+        {movies?.length > 0 ? (
+          movies.map((movie) => (
+            <a
+              href={`https://www.imdb.com/title/${movie.imdbID}`}
+              target="_blank"
+              key={movie.imdbID}
+            >
+              <MovieCard key={movie.imdbID} movie={movie} />
+            </a>
+          ))
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+export default function MovieCard({ movie }) {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={
+            movie.Poster === "N/A"
+              ? "https://via.placeholder.com/300x450"
+              : movie.Poster
+          }
+          alt={movie.Title}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+}

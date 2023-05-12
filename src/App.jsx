b4aa54470901a0732a8f30cbfef09890a036b580
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-05-12 18:22:57 +0530

    Update API_URL to use https

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 import MovieCard from "./components/MovieCard";
 
-const API_URL = `http://www.omdbapi.com/?apikey=${
+const API_URL = `https://www.omdbapi.com/?apikey=${
   import.meta.env.VITE_API_KEY
 }`;
 
